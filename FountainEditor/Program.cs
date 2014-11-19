64601c4f68254fbdf7deafdcd58294d53bdde1c1
nnelgxorz <nnelgxorz@gmail.com>
2014-11-18 22:18:12 -0700

    Program Inputs and Outputs Files for Testing

--- FountainEditor/Program.cs
+++ FountainEditor/Program.cs
@@ -6,12 +6,23 @@ namespace FountainEditor
     class Program
     {
         static void Main(params string[] args)
-        {
+        { 
+            var text = new StreamReader(@"C:\Users\Glenn\Desktop\test\screenplayIN.txt");
+            //var reader = new StreamReader(@"C:\Users\Glenn\Desktop\test\BigFishScript.txt");
+
             var tokenizer = new Tokenizer();
-            var tokens = tokenizer.Parse(string.Empty);
+            var tokens = tokenizer.Parse(text.ReadToEnd());
 
             var optimizer = new Optimizer();
             optimizer.Optimize(tokens);
+
+            var writer = new StreamWriter(@"C:\Users\Glenn\Desktop\test\screenplayOUT.txt");
+
+            foreach (var element in tokens)
+            {
+                writer.Write(element.Text);
+            }
+            writer.Flush();
         }
     }
 }
