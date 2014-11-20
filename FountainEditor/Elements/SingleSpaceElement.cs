5d452de6604ef55413d8b85777e7b65435e88583
nnelgxorz <nnelgxorz@gmail.com>
2014-11-20 12:21:14 -0700

    New Element for SingleSpaces

---
+++ FountainEditor/Elements/SingleSpaceElement.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FountainEditor.Elements
+{
+    public class SingleSpaceElement : Element
+    {
+        public SingleSpaceElement(string text)
+            : base(text)
+        {
+        }
+
+        public override string Print()
+        {
+            return string.Format("<ds>{0}</ds>", Text);
+        }
+    }
+}
