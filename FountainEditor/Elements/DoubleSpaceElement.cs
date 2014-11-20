007587e33d33c95d095b79399d399ec4f6371780
nnelgxorz <nnelgxorz@gmail.com>
2014-11-20 11:32:12 -0700

    New Double Space Element

---
+++ FountainEditor/Elements/DoubleSpaceElement.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FountainEditor.Elements
+{
+    public class DoubleSpaceElement : Element
+    {
+        public DoubleSpaceElement(string text)
+            : base(text)
+        {
+        }
+
+        public override string Print()
+        {
+            return string.Format("<ds>{0}</ds>", Text);
+        }
+    }
+}
