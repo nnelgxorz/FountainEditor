cb6a738246c8e8f8ac7b7143d7e95ad3fab1a293
nnelgxorz <nnelgxorz@gmail.com>
2014-11-20 14:47:27 -0700

    New Element for Tabs

---
+++ FountainEditor/Elements/TabElement.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FountainEditor.Elements
+{
+    public class TabElement : Element
+    {
+        public TabElement(string text)
+            : base(text)
+        {
+        }
+
+        public override string Print()
+        {
+            return string.Format("<t>{0}</t>", Text);
+        }
+    }
+}
