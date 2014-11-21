ad396608c05e22c86f6daf7e26fd9a80c295ebaa
nnelgxorz <nnelgxorz@gmail.com>
2014-11-20 23:37:16 -0700

    TitlePage Key and Value Elements

---
+++ FountainEditor/Elements/TitlePageValue.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FountainEditor.Elements
+{
+    public class TitlePageValue : Element
+    {
+        public TitlePageValue(string text)
+            : base(text)
+        {
+        }
+
+        public override string Print()
+        {
+            return string.Format("<tv>{0}</tv>", Text);
+        }
+    }
+}
