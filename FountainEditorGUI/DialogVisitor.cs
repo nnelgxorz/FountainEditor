1b4994c08f57061571c1881c0329c04b72f74003
nnelgxorz <nnelgxorz@gmail.com>
2014-12-05 00:32:26 -0700

    DialogueVisitor For Dialogue Formatting

---
+++ FountainEditorGUI/DialogVisitor.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Documents;
+
+namespace FountainEditorGUI
+{
+    class DialogVisitor : FountainEditor.FountainBaseVisitor<Paragraph>
+    {
+        public override Paragraph VisitCharacter(FountainEditor.FountainParser.CharacterContext context)
+        {
+            string text = context.GetText();
+            Run r = new Run(text);
+            Paragraph p = new Paragraph();
+            p.Margin = new System.Windows.Thickness(370, 0, 100, 0);
+            p.Inlines.Add(r);
+            return p;
+        }
+        public override Paragraph VisitSpan(FountainEditor.FountainParser.SpanContext context)
+        {
+            string text = context.GetText();
+            Run r = new Run(text);
+            Paragraph p = new Paragraph();
+            p.Margin = new System.Windows.Thickness(250,0,250,0);
+            p.Inlines.Add(r);
+            return p;
+        }
+        public override Paragraph VisitParenthetical(FountainEditor.FountainParser.ParentheticalContext context)
+        {
+            string text = context.GetText();
+            Run r = new Run(text);
+            Paragraph p = new Paragraph();
+            p.Margin = new System.Windows.Thickness(310, 0, 290, 0);
+            p.Inlines.Add(r);
+            return p;
+        }
+
+        public override Paragraph VisitBlankLine(FountainEditor.FountainParser.BlankLineContext context)
+        {
+            string text = context.GetText();
+            Run r = new Run(text);
+            Paragraph p = new Paragraph();
+            p.Inlines.Add(r);
+            return p;
+        }
+
+    }
+}
