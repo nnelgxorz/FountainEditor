5b50fbe8b64bf1b7675eea09ff78079dfe6a152e
Adam Becker <beannaich@gmail.com>
2015-02-10 22:20:27 -0700

    Add the things I'll need to begin making a view model for the main window.

---
+++ FountainEditorGUI/Commands/RelayCommandGeneric.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace FountainEditorGUI.Commands {
+    public sealed class RelayCommand<T> : ICommand {
+        private Action<T> execute;
+        private Func<T, bool> canExecute;
+
+        public event EventHandler CanExecuteChanged {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public RelayCommand(Action<T> execute) {
+            this.execute = execute;
+            this.canExecute = e => true;
+        }
+
+        public RelayCommand(Action<T> execute, Func<T, bool> canExecute) {
+            this.execute = execute;
+            this.canExecute = canExecute;
+        }
+
+        public bool CanExecute(object parameter) {
+            return canExecute((T)parameter);
+        }
+
+        public void Execute(object parameter) {
+            execute((T)parameter);
+        }
+    }
+}
