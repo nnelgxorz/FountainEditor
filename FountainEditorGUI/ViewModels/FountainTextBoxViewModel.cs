b2a4a961e75d534e8daf75f8e48d9a00f78f3ad4
Adam Becker <beannaich@gmail.com>
2015-02-11 19:19:02 -0700

    Add 'FountainTextBox' to replace the editing text box on the main window.

---
+++ FountainEditorGUI/ViewModels/FountainTextBoxViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System.Windows.Documents;
+using FountainEditor.Messaging;
+using FountainEditorGUI.Messages;
+
+namespace FountainEditorGUI.ViewModels
+{
+    public sealed class FountainTextBoxViewModel : ViewModelBase
+    {
+        private FlowDocument document;
+
+        public FlowDocument Document
+        {
+            get { return document; }
+            set
+            {
+                if (this.document != value)
+                {
+                    this.document = value;
+
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public FountainTextBoxViewModel(IMessagePublisher<DocumentMessage> documentMessagePublisher)
+        {
+            documentMessagePublisher.Subscribe(DocumentChanged);
+        }
+
+        private void DocumentChanged(DocumentMessage message)
+        {
+            this.Document = message.Document;
+        }
+    }
+}
