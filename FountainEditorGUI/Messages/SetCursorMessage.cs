6b99038a98a96134a057efb4b1e407a32883f4e9
Glenn Becker <nnelgxorz@gmail.com>
2015-03-20 20:42:54 -0600

    Navigate Doc From Outliner / Set Cursor Message

---
+++ FountainEditorGUI/Messages/SetCursorMessage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FountainEditorGUI.Messages
+{
+    public sealed class SetCursorMessage
+    {
+        public int index;
+        public int offset;
+
+        public SetCursorMessage(int index, int offset)
+        {
+            this.index = index;
+            this.offset = offset;
+        }
+    }
+}
---
+++ FountainEditorGUI/Messages/outlinerNavigationMessage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FountainEditorGUI.Messages
+{
+    public sealed class OutlinerNavigationMessage
+    {
+        public string text { get; private set; }
+
+        public OutlinerNavigationMessage(string text)
+        {
+            this.text = text;
+        }
+    }
+}
