f07e69e8b571a7e438467ccdb5672b29ba4b5748
Glenn Becker <nnelgxorz@gmail.com>
2015-03-20 20:50:29 -0600

    Formatting Utilities for Outlining Nesting

---
+++ FountainEditorGUI/AddHashtags.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FountainEditorGUI
+{
+    public sealed class AddHashtags
+    {
+        public string Add (string text, int amount)
+        {
+            for (int i = 0; i < amount; i++)
+            {
+                text = string.Format("#{0}", text);
+            }
+
+            return text;
+        }
+    }
+}
---
+++ FountainEditorGUI/RemoveHashtags.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FountainEditorGUI
+{
+    public sealed class RemoveHashtags
+    {
+        public string Remove(string text, int amount)
+        {
+            for (int i = 0; i < amount; i++)
+            {
+                text.Substring(1, text.Length);
+            }
+
+            return text;
+        }
+    }
+}
