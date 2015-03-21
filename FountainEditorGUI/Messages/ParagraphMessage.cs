eb50ffe34072b4b3c0f261117302047a82cf2aa0
Glenn Becker <nnelgxorz@gmail.com>
2015-03-20 20:52:17 -0600

    Paragraph Parsing Message

---
+++ FountainEditorGUI/Messages/ParagraphMessage.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Windows.Documents;
+using FountainEditor.ObjectModel;
+
+namespace FountainEditorGUI.Messages
+{
+    public sealed class ParagraphMessage
+    {
+        public string Paragraph { get; private set; }
+        
+        public ParagraphMessage(string paragraph)
+        {
+            this.Paragraph = paragraph;
+        }
+    }
+}
