da912c4be985156e54e1a212ff3de4cf09cb2dda
Glenn Becker <nnelgxorz@gmail.com>
2015-03-22 16:57:00 -0600

    Message For Setting Selected Outliner Item

---
+++ FountainEditorGUI/Messages/OutlinerSelectionMessage.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FountainEditorGUI.Messages
+{
+    public sealed class OutlinerSelectionMessage
+    {
+        public string text;
+        public OutlinerSelectionMessage(string text)
+        {
+            this.text = text;
+        }
+    }
+}
