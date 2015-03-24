bf1912675bd51db8cb0a885f48b548ffc10bc61f
Glenn Becker <nnelgxorz@gmail.com>
2015-03-24 14:09:21 -0600

    Streamlining Outliner Drag Drop Logic

---
+++ FountainEditorGUI/OutlinerDragDropLogic.cs
@@ -0,0 +1,65 @@
+﻿using FountainEditor.Messaging;
+using FountainEditorGUI.Messages;
+using System.Collections.ObjectModel;
+using System.Linq;
+
+namespace FountainEditorGUI
+{
+    public sealed class OutlinerDragDropLogic
+    {
+        private GenerateObservableCollectionFromHierarchy generateCollectionHierarchy;
+        private RemoveElementsFromObservableCollection removeElementsFromCollection;
+        private InsertObservableCollectionIntoObservableCollection insertCollectionInCollection;
+        private GetEndOfHierarchicalElementsIndex getEndOfHierarchyIndex;
+        private GetEndOfOutlineSectionIndex getEndOfSectionIndex;
+        private CollectionItemsNestingService nestCollectionItems;
+        private IMessagePublisher<OutlinerSelectionMessage> outlinerSelectionMessage;
+
+        public OutlinerDragDropLogic(
+            GenerateObservableCollectionFromHierarchy generateCollectionHierarchy,
+            RemoveElementsFromObservableCollection removeElementsFromCollection,
+            InsertObservableCollectionIntoObservableCollection insertCollectionInCollection,
+            GetEndOfHierarchicalElementsIndex getEndOfHierarchyIndex,
+            GetEndOfOutlineSectionIndex getEndOfSectionIndex,
+            CollectionItemsNestingService nestCollectionItems,
+            IMessagePublisher<OutlinerSelectionMessage> outlinerSelectionMessage)
+        {
+            this.generateCollectionHierarchy = generateCollectionHierarchy;
+            this.removeElementsFromCollection = removeElementsFromCollection;
+            this.insertCollectionInCollection = insertCollectionInCollection;
+            this.getEndOfHierarchyIndex = getEndOfHierarchyIndex;
+            this.getEndOfSectionIndex = getEndOfSectionIndex;
+            this.nestCollectionItems = nestCollectionItems;
+            this.outlinerSelectionMessage = outlinerSelectionMessage;
+        }
+
+        public ObservableCollection<string> DoDrop(ObservableCollection<string> collection, DragDropMessage message)
+        {
+            int insertIndex = message.dropIndex - 1;
+            int amount = message.dropItemDepth - message.dragItemDepth;
+            ObservableCollection<string> selection = generateCollectionHierarchy.Generate(collection, message.dragIndex, message.dragItemDepth);
+
+            if (message.dropAction.Equals("After") || message.dropAction.Equals("Nest"))
+            {
+                insertIndex = getEndOfHierarchyIndex.GetIndex(collection, message.dragItemDepth, message.dropIndex);
+            }
+
+            selection = nestCollectionItems.DoNesting(selection, amount, message.dropAction);
+
+            if (message.dragIndex > message.dropIndex)
+            {
+                removeElementsFromCollection.Remove(collection, message.dragIndex, selection.Count);
+                insertCollectionInCollection.Insert(collection, selection, insertIndex);
+            }
+            else
+            {
+                insertCollectionInCollection.Insert(collection, selection, insertIndex);
+                removeElementsFromCollection.Remove(collection, message.dragIndex, selection.Count);
+            }
+
+            outlinerSelectionMessage.Publish(new OutlinerSelectionMessage(selection.ElementAt(0)));
+            selection.Clear();
+            return collection;
+        }
+    }
+}
---
+++ FountainEditorGUI/SectionParagraphsNestingService.cs
@@ -0,0 +1,46 @@
+﻿using System.Linq;
+using System.Windows.Controls;
+using System.Windows.Documents;
+
+namespace FountainEditorGUI
+{
+    public class SectionParagraphsNestingService
+    {
+        private AddHashtags addHashTags;
+        private RemoveHashtags removeHashTags;
+        public SectionParagraphsNestingService(AddHashtags addHashTags, RemoveHashtags removeHashTags)
+        {
+            this.addHashTags = addHashTags;
+            this.removeHashTags = removeHashTags;
+        }
+        public FlowDocument DoNesting(RichTextBox textBox, int start, int stop, int amount, string dropAction)
+        {
+            if (amount == 0 && !(dropAction.Equals("Nest")))
+            {
+                return textBox.Document;
+            }
+
+            for (int i = start; i < stop; i++)
+            {
+                Paragraph paragraph = (Paragraph)textBox.Document.Blocks.ElementAt(i);
+                string paragraphText = new TextRange(paragraph.ContentStart, paragraph.ContentEnd).Text;
+                string text = null;
+
+                if (paragraphText.StartsWith("#"))
+                {
+                    if (amount < 0)
+                    {
+                        text = removeHashTags.Remove(paragraphText, amount * -1);
+                    }
+                    else
+                    {
+                        text = addHashTags.Add(paragraphText, amount + 1);
+                    }
+                    paragraph.Inlines.Clear();
+                    paragraph.Inlines.Add(new Run(text));
+                }
+            }
+            return textBox.Document;
+        }
+    }
+}
